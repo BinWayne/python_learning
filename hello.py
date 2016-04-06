ca501f13145a611907974ebcbdb0b9f36ef92b4d
BinWayne <36578067@qq.com>
2016-04-06 15:40:19 +0800

    Signed-off-by: BinWayne <36578067@qq.com>

---
+++ hello.py
@@ -0,0 +1,18 @@
+print ('hello world')
+
+a = 45678+0x12fd2
+print (a);
+
+a = 'Learn python'
+print (a);
+
+print(100<99)
+print(0xff == 255)
+print ('400','456')
+
+x1 = 1
+d = 3
+n = 100
+x100 = x1+(n-1)*d
+s = (x1+x100)*100/2
+print (s)
---
+++ hello_world.py
@@ -0,0 +1,12 @@
+# hello.py
+# -*- coding: utf-8 -*-
+print 'hello world'
+
+print 'hello','world','test'
+
+#name = raw_input('please type something: ')
+#print name;
+
+#u'关于 占位符测试
+print 'hi,%s'%'tony'
+# hi,tony
